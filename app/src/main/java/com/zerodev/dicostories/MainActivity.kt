daa34a81f04f8952c859637238b5d16a0614030c
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2022-08-26 20:32:51 +0700

    upgrade sdkVersion & hide systemUI in splash

--- app/src/main/java/com/zerodev/dicostories/MainActivity.kt
+++ app/src/main/java/com/zerodev/dicostories/MainActivity.kt
@@ -3,6 +3,9 @@ package com.zerodev.dicostories
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.zerodev.dicostories.view.auth.LoginActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,12 +16,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hideSystemUI()
         supportActionBar?.hide()
 
         CoroutineScope(Dispatchers.Main).launch {
             delay(3000L)
             startActivity(Intent(this@MainActivity, LoginActivity::class.java))
             finish()
+        }
+    }
+
+    private fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
 }
